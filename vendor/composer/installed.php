5ba6482b6875184bd056ae8917346557955581b4
makaravich <d.makarski@gmail.com>
2025-03-22 13:50:58 +0100

    Add Composer support

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'makaravich/simple-tg-bot',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '019ec8867bf78f3a3b8b2366ba4920ba7814590c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'makaravich/simple-tg-bot' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '019ec8867bf78f3a3b8b2366ba4920ba7814590c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
