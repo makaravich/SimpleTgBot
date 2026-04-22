6a2f9a14cbe4ec5f260df3dbf0b97f3c3e19600f
Dzmitry Makarski <d.makarski@gmail.com>
2026-04-22 17:02:13 +0200

    V. 0.1.20 - Add function send_stars_invoice

--- src/Simple_Tg_Bot.php
+++ src/Simple_Tg_Bot.php
@@ -3,7 +3,7 @@
 /**
  * This class allows you to interact with Telegram Bot API
  *
- * V. 0.1.19
+ * V. 0.1.20
  */
 class Simple_Tg_Bot
 {
@@ -406,6 +406,43 @@ class Simple_Tg_Bot
             'chat_id' => $chat_id,
             'document' => new CURLFile($document_path),
             'caption' => $caption
+        ];
+
+        return $this->send_request($url, $data);
+    }
+
+    /**
+     * Sending an invoice in the Telegram Stars
+     *
+     * @param $title
+     * @param $description
+     * @param $payload
+     * @param $stars_amount
+     * @param string $chat_id
+     * @return mixed
+     */
+    function send_stars_invoice($title, $description, $payload, $stars_amount, string $chat_id = ''): mixed {
+        if ($chat_id === '') {
+            $chat_id = $this->chat_id;
+        }
+
+        $url = $this->api_url . "sendInvoice";
+
+        $data = [
+            'chat_id' => $chat_id,
+            'title' => $title,
+            'description' => $description,
+            'payload' => $payload,     // For example, "buy:premium_30d:user123"
+            'currency' => 'XTR',
+            'prices' => json_encode([
+                [
+                    'label' => $title,
+                    'amount' => $stars_amount
+                ]
+            ], JSON_UNESCAPED_UNICODE),
+            'need_name' => false,
+            'need_phone_number' => false,
+            'need_email' => false,
         ];
 
         return $this->send_request($url, $data);
